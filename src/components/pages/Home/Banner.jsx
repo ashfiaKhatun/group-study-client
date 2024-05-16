0b4a40e70ccd6f01fc457d533abbb822c411bff3
ashfiaKhatun <ashfia1204@gmail.com>
2024-05-16 10:54:35 +0600

    banner section modified

--- src/components/pages/Home/Banner.jsx
+++ src/components/pages/Home/Banner.jsx
@@ -3,12 +3,11 @@ const Banner = () => {
     return (
         <div>
             <div className="hero bg-cover bg-no-repeat my-8" style={{ backgroundImage: 'url(https://i.ibb.co/Xsd5ftK/photo-1522202176988-66273c2fd55f.jpg)' }}>
-                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-overlay bg-opacity-70"></div>
                 <div className="hero-content text-center text-neutral-content py-36">
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold">Connect Students</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <p className="mb-5">Struggling to go it alone? Connect Students helps you find or form a study group to crush your classes and make friends along the way. Sign up and start connecting today!</p>
                     </div>
                 </div>
             </div>
