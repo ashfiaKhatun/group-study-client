42725c342e8977bea2cacd93d3b98971828e2319
ashfiaKhatun <ashfia1204@gmail.com>
2024-05-16 22:32:31 +0600

    mark giving section updated

--- src/components/pages/PendingAssignments/PendingAssignmentMarks.jsx
+++ src/components/pages/PendingAssignments/PendingAssignmentMarks.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const PendingAssignmentMarks = () => {
 
@@ -7,19 +8,65 @@ const PendingAssignmentMarks = () => {
 
     const { link, note } = loader;
 
+    const handleMarksSubmit = e =>{
+        e.preventDefault();
+
+        const form = e.target;
+
+        const marksInput = form.marksInput.value;
+        const feedBack = form.feedBack.value;
+
+        const sendFeedBack = { marksInput, feedBack};
+
+        fetch('https://group-study-server-three.vercel.app/all-assignments', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(sendFeedBack)
+        })
+            .then(res => res.json())
+            .then(() => {
+                Swal.fire({
+                    title: "Success",
+                    text: "FeedBack provided Successfully",
+                    icon: "success"
+                });
+            })
+
+
+
+
+        form.reset();
+    }
+
+
+
     return (
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-3 p-2 md:p-12">
+
+            <div className="card card-compact bg-gray-100 shadow-xl border-b-8 border-l-8">
+
+                <div className="card-body">
+                    <h2 className="card-title">Submitted Link: </h2>
+                    <p onClick={() => document.getElementById('my_modal_3').showModal()} className="cursor-pointer truncate">{link}</p>
+                    <h2 className="font-bold text-lg">Note: </h2>
+                    <p>{note}</p>
+
+                    <form onSubmit={handleMarksSubmit}>
+                        <div>
+                            <label className="label-text font-bold">Mark</label>
+                            <input type="text" name="mark"placeholder="Mark"className="input  w-full mt-2" />
+                        </div>
+
+                        <div>
+                            <label className="label-text font-bold">Feedback</label>
+                            <input type="text" name="feedback" placeholder="Feedback" className="input w-full mt-2" />
+                        </div>
+                    </form>
 
 
-            <div className="border rounded-xl shadow-lg hover:shadow-2xl space-y-2 p-4 m-2 lg:m-4 border-l-8 border-b-8">
-
-                <div className="">
-                    <p className=""><b>Submitted Assignment Link:</b> <span className="cursor-pointer" onClick={() => document.getElementById('my_modal_3').showModal()}>{link}</span></p>
                 </div>
-                <p><b>Note:</b> {note}</p>
-
-
-
             </div>
 
             {/* <div>
@@ -28,17 +75,20 @@ const PendingAssignmentMarks = () => {
             </div> */}
 
 
-            <dialog id="my_modal_3" className="modal">
-                <div className="modal-box h-[400px] w-[950px]">
-                    <form method="dialog">
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                    </form>
-                    <iframe src={link} className="h-[300px] w-[850px]"></iframe>
+            <div className="">
+                <dialog id="my_modal_3" className="modal">
+                    <div className="modal-box max-w-5xl flex">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        </form>
+                        <iframe src={link} className="h-[500px] w-full lg:w-[950px]"></iframe>
 
 
-                </div>
-            </dialog>
+                    </div>
+                </dialog>
+
+            </div>
 
         </div>
     );
